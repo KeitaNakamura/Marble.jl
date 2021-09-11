aab8fdc40eee1d908630a33e849d41ea96d7855a
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-09-11 11:16:30 +0900

    Refactoring

--- src/MaterialModels/DruckerPrager.jl
+++ src/MaterialModels/DruckerPrager.jl
@@ -56,16 +56,13 @@ function update_stress(model::DruckerPrager{<: Any, <: SoilElastic}, σ::Symmetr
     ϵᵉ = ϵᵉ_trial
     Δγ = zero(T)
     for i in 1:20
-        σ = ∇W(model.elastic, ϵᵉ)
-        # dfdσ, f = gradient(σ -> yield_function(model, σ), σ, :all)
-        f = yield_function(model, σ)
-        dfdσ = gradient(σ -> yield_function(model, σ), σ)
+        Dᵉ, σ = gradient(ϵᵉ -> ∇W(model.elastic, ϵᵉ), ϵᵉ, :all)
+        dfdσ, f = gradient(σ -> yield_function(model, σ), σ, :all)
         dgdσ = plastic_flow(model, σ)
 
         R = ϵᵉ - ϵᵉ_trial + Δγ*dgdσ
         norm(R) < sqrt(eps(T)) && abs(f) < sqrt(eps(T)) && break
 
-        Dᵉ = ∇²W(model.elastic, ϵᵉ)
         dfdσ_Dᵉ = dfdσ ⊡ Dᵉ
         dΔγ = (f - dfdσ_Dᵉ ⊡ R) / (dfdσ_Dᵉ ⊡ dgdσ)
 
@@ -75,39 +72,6 @@ function update_stress(model::DruckerPrager{<: Any, <: SoilElastic}, σ::Symmetr
     end
     σ
 end
-
-#=
-function residual_vector(model::DruckerPrager, x, ϵᵉ_trial)
-    @inbounds begin
-        σ = frommandel(SymmetricSecondOrderTensor{3}, view(x, 1:6))
-        Δγ = x[7]
-    end
-    ϵᵉ = compute_elastic_strain(model.elastic, σ)
-    f = yield_function(model, σ)
-    dgdσ = plastic_flow(model, σ)
-    Vec{7}(Tuple(tomandel(ϵᵉ_trial - ϵᵉ - Δγ*dgdσ))..., f)
-end
-
-function update_stress(model::DruckerPrager{<: Any, <: SoilElastic}, σ::SymmetricSecondOrderTensor{3, T}, dϵ::SymmetricSecondOrderTensor{3})::typeof(dϵ) where {T}
-    # compute the stress at the elastic trial state
-    ϵᵉ = compute_elastic_strain(model.elastic, σ)
-    ϵᵉ_trial = ϵᵉ + dϵ
-    σ_trial = compute_stress(model.elastic, ϵᵉ_trial)
-    yield_function(model, σ_trial) ≤ 0.0 && return σ_trial
-
-    # prepare solution vector x
-    x = Vec{7}(Tuple(tomandel(σ_trial))..., zero(T))
-
-    # plastic corrector
-    for i in 1:20 # should be converged in 20 iterations
-        J, r = gradient(x -> residual_vector(model, x, ϵᵉ_trial), x, :all)
-        norm(r) < sqrt(eps(T)) && break
-        # update states
-        x += -J \ r
-    end
-    @inbounds frommandel(SymmetricSecondOrderTensor{3}, view(x, 1:6))
-end
-=#
 
 function yield_function(model::DruckerPrager, σ::SymmetricSecondOrderTensor{3})::eltype(σ)
     A = model.A
