1537ff3c98aa6c3d3fefb544d6621d7dfe003b96
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-10-05 09:52:58 +0900

    Overload `add_field_data` instead of `vtk_point_data` for the use of `setindex!`

--- src/vtk.jl
+++ src/vtk.jl
@@ -57,12 +57,7 @@ function WriteVTK.vtk_grid(vtk::AbstractString, grid::Grid)
     vtk_grid(vtk, map(collect, gridaxes(grid))...)
 end
 
-"""
-    vtk_point_data(vtk, data::AbstractVector{<: Vec}, name)
-
-Write the vector field data to the `vtk` file.
-"""
-function WriteVTK.vtk_point_data(vtk::WriteVTK.DatasetFile, data::AbstractVector{<: Tensor}, name::AbstractString)
+function WriteVTK.add_field_data(vtk::WriteVTK.DatasetFile, data::AbstractVector{<: Tensor}, name::AbstractString, ::WriteVTK.VTKPointData)
     vtk_point_data(vtk, vtk_format(data), name)
 end
 
