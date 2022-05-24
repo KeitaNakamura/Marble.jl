45374ccfa2d9d8212a57f1d172a1ed69197b5433
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-05-24 18:19:59 +0900

    Cleanup GIMP calculation

--- src/Interpolations/gimp.jl
+++ src/Interpolations/gimp.jl
@@ -1,58 +1,54 @@
 struct GIMP <: Kernel end
 
 getsupportlength(::GIMP, l) = 1.0 .+ l # `l` must be normalized by `dx`
-
 @pure getnnodes(f::GIMP, ::Val{dim}) where {dim} = prod(nfill(3, Val(dim)))
 
-@inline function value(::GIMP, ξ::Real, l::Real) # `l` is normalized radius
+# simple GIMP calculation
+# See Eq.(40) in
+# Bardenhagen, S. G., & Kober, E. M. (2004).
+# The generalized interpolation material point method.
+# Computer Modeling in Engineering and Sciences, 5(6), 477-496.
+# boundary treatment is ignored
+function value(::GIMP, ξ::Real, l::Real) # `l` is normalized radius
     ξ = abs(ξ)
     ξ < l   ? 1 - (ξ^2 + l^2) / 2l :
     ξ < 1-l ? 1 - ξ                :
     ξ < 1+l ? (1+l-ξ)^2 / 4l       : zero(ξ)
 end
+@inline value(f::GIMP, ξ::Vec, l::Vec) = prod(maptuple(value, f, Tuple(ξ), Tuple(l)))
 
+# used in `WLS`
 # `x` and `l` must be normalized by `dx`
 @inline function Base.values(::GIMP, x::T, l::T) where {T <: Real}
     V = Vec{3, T}
     x′ = fract(x - T(0.5))
     ξ = x′ .- V(-0.5, 0.5, 1.5)
-    value.((GIMP(),), ξ, l)
+    maptuple(value, GIMP(), Tuple(ξ), Tuple(l))
 end
 
+# used in `KernelCorrection`
 # `x` and `l` must be normalized by `dx`
 _gradient_GIMP(x, l) = gradient(x -> value(GIMP(), x, l), x, :all)
-@generated function values_gradients(::GIMP, x::T, l::T) where {T <: Real}
-    exps = [:(_gradient_GIMP(ξ[$i], l)) for i in 1:3]
-    quote
-        @_inline_meta
-        V = Vec{3, T}
-        x′ = fract(x - T(0.5))
-        ξ = x′ .- V(-0.5, 0.5, 1.5)
-        vals_grads = tuple($(exps...))
-        Vec($([:(vals_grads[$i][2]) for i in 1:3]...)), Vec($([:(vals_grads[$i][1]) for i in 1:3]...))
-    end
+function _values_gradients(::GIMP, x::T, l::T) where {T <: Real}
+    V = Vec{3, T}
+    x′ = fract(x - T(0.5))
+    ξ = x′ .- V(-0.5, 0.5, 1.5)
+    vals_grads = maptuple(_gradient_GIMP, Tuple(ξ), l)
+    vals  = maptuple(getindex, vals_grads, 2)
+    grads = maptuple(getindex, vals_grads, 1)
+    Vec(vals), Vec(grads)
 end
-
 @generated function values_gradients(::GIMP, x::Vec{dim}, l::Vec{dim}) where {dim}
-    exps = [:(values_gradients(GIMP(), x[$i], l[$i])) for i in 1:dim]
-    derivs = map(1:dim) do i
+    exps = map(1:dim) do i
         x = [d == i ? :(grads[$d]) : :(vals[$d]) for d in 1:dim]
         :(Tuple(otimes($(x...))))
     end
     quote
         @_inline_meta
-        vals_grads = tuple($(exps...))
-        vals = getindex.(vals_grads, 1)
-        grads = getindex.(vals_grads, 2)
-        Tuple(otimes(vals...)), Vec{dim}.($(derivs...))
-    end
-end
-
-@generated function value(f::GIMP, ξ::Vec{dim, T}, l::Vec{dim}) where {dim, T}
-    exps = [:(value(f, ξ[$i], l[$i])) for i in 1:dim]
-    quote
-        @_inline_meta
-        *($(exps...))
+        vals_grads = maptuple(_values_gradients, GIMP(), Tuple(x), Tuple(l))
+        vals  = maptuple(getindex, vals_grads, 1)
+        grads = maptuple(getindex, vals_grads, 2)
+        Tuple(otimes(vals...)), maptuple(Vec, $(exps...))
     end
 end
 
